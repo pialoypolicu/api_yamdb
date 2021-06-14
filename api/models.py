3dd4419893432a8c563c5c96cedbe49e4ddada5c
pialoypolicu <paryashijvnebe@gmail.com>
2021-06-14 13:42:03 +0300

    add admin site titles add model titles

--- api/models.py
+++ api/models.py
@@ -5,4 +5,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Titles(models.Model):
-    pass
+    title = models.CharField('Name', max_length=200, help_text='Введи название')
+    # category = models.ForeignKey(Catigories, on_delete=models.CASCADE, related_name='категория')
+    year = models.SmallIntegerField('Year', help_text='Год выхода', null=True)
+
+
+    class Meta:
+        verbose_name_plural = 'Titles'
