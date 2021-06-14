8d8b0efb044449ea191aeee1b29b577e84484baa
pialoypolicu <paryashijvnebe@gmail.com>
2021-06-14 20:45:15 +0300

    Create pagination default in settings and add in views.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
 from api.models import Title
 from api.serializers import TitleSerializer
@@ -10,3 +11,4 @@ class TitleViewSet(ModelViewSet):
     serializer_class = TitleSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['category', 'year', 'name']  # 'genre'
+    pagination_class = PageNumberPagination
