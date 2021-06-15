0d7a8aae58049cb6d92a37011baf35701cf63018
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-06-15 14:27:17 +0300

    UserViewSet added.

--- api/views.py
+++ api/views.py
@@ -1,10 +1,44 @@
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import filters
+from rest_framework import filters, status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 
-from api.models import Title
-from api.serializers import TitleSerializer
+from api.models import Title, User
+from api.permissions import IsAdmin
+from api.serializers import TitleSerializer, UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAdmin, IsAdminUser)
+
+    @action(
+        detail=False,
+        methods=['get'],
+        permission_classes=(IsAuthenticated,)
+    )
+    def me(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    @me.mapping.patch
+    def patch_me(self, request):
+        user = request.user
+        if user.role != User.Roles.ADMIN:
+            request.data.pop('role', None)
+        serializer = self.serializer_class(
+            user, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TitleViewSet(ModelViewSet):
