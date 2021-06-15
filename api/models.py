52e1f55b26cdf5f99c0eff458bd6237a28671ecb
pialoypolicu <paryashijvnebe@gmail.com>
2021-06-15 11:11:40 +0300

    Create search, update Title model

--- api/models.py
+++ api/models.py
@@ -21,9 +21,12 @@ class User(AbstractUser):
 
 class Title(models.Model):
     name = models.CharField('Name', max_length=200, help_text='Введи название')
-    # category = models.ForeignKey(Catigories, related_name='title')
-    category = models.IntegerField('Категория', )
     year = models.SmallIntegerField('Year', help_text='Год выхода', null=True)
+    description = models.TextField('Описание', null=True)
+    # category = models.ForeignKey(Catigories, related_name='title')
+    genre = models.CharField('Жанр', max_length=50, null=True)
+    category = models.IntegerField('Категория', null=True)
+
 
     def __str__(self):
         return self.name
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 
@@ -9,6 +10,7 @@ from api.serializers import TitleSerializer
 class TitleViewSet(ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['category', 'year', 'name']  # 'genre'
+    search_fields = ['name', ]
     pagination_class = PageNumberPagination
