c293ba3300f2fa7e4de49065910685bc9fbc6a7e
pialoypolicu <paryashijvnebe@gmail.com>
2021-06-15 13:43:53 +0300

    Create Review in admin, model, serializer, url, viewset

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Title
+from api.models import Title, Review
 
 
 class TitleSerializer(serializers.ModelSerializer):
@@ -8,3 +8,9 @@ class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Title
+
+
+class ReviewSerilizer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Review
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import TitleViewSet
+from api.views import TitleViewSet, ReviewViewSet
 
 router_yamdb_v1 = DefaultRouter()
 router_yamdb_v1.register('titles', TitleViewSet, basename='titles_view')
+router_yamdb_v1.register(r'titles/(?P<id>[^/.]+)/review', ReviewViewSet)
 
 urlpatterns = [
     path('v1/', include(router_yamdb_v1.urls))
