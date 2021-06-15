3173a86dc4717d7251d55df2ec40a61056ca0965
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-06-15 13:08:15 +0300

    UserSerializer added.

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 
-from api.models import Title
+from api.models import Title, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    bio = serializers.CharField(source='description')
+
+    class Meta:
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'bio',
+            'email',
+            'role',
+        )
+        model = User
 
 
 class TitleSerializer(serializers.ModelSerializer):
-
     class Meta:
         fields = '__all__'
         model = Title
