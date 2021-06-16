13d09144e68b3264e77d8654e3fce4f2ae47ac19
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-06-16 12:21:01 +0300

    ReadOnly and IsAdminOrReadOnly permissions added.

--- api/permissions.py
+++ api/permissions.py
@@ -3,6 +3,17 @@ from rest_framework import permissions
 from api.models import User
 
 
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.role == User.Roles.ADMIN
+                or request.method in permissions.SAFE_METHODS)
+
+
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == User.Roles.ADMIN
